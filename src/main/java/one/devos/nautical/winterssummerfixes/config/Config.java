51c31a5669c17c3e8817c820f30d58a1caa10ffc
Asoji <asoji-git@hiitsdevin.mozmail.com>
2025-07-29 10:53:18 -0700

    ✏️ more stuff to config

--- src/main/java/one/devos/nautical/winterssummerfixes/config/Config.java
+++ src/main/java/one/devos/nautical/winterssummerfixes/config/Config.java
@@ -4,30 +4,39 @@ import eu.midnightdust.lib.config.MidnightConfig;
 
 public class Config extends MidnightConfig {
     public static String painAndSufferingAndSufferingAndPain = "I understand what I am doing will cause potential instabilities with Flashback. I will not report this to Flashback support. Praise Winter. Praise Moulberry. Praise Ishland.";
+    public static final String limitsGrappleCategory = "limitsGrapple";
+    public static final String flashbackCategory = "flashback";
+    public static final String limitsGrappleModId = "limits_grapple";
+    public static final String flashbackModId = "flashback";
 
-    @Entry(category = "limitsGrapple", isColor = true)
+    @Comment(category = limitsGrappleCategory)
+    public static String limitsGrappleSwingColorWindowMissingWarning;
+
+    @Entry(category = limitsGrappleCategory, isColor = true, width = 7, min = 7)
+    @Condition(requiredModId = limitsGrappleModId)
     public static String limitsGrappleHitColor = "#3333ff";
 
-    @Entry(category = "limitsGrapple", isColor = true)
+    @Entry(category = limitsGrappleCategory, isColor = true, width = 7, min = 7)
+    @Condition(requiredModId = limitsGrappleModId)
     public static String limitsGrappleMissColor = "#99994c";
 
-    @Comment(category = "flashback")
-    @Condition(requiredModId = "flashback")
+    @Comment(category = flashbackCategory)
+    @Condition(requiredModId = flashbackModId)
     public static String flashbackReplayForceAllowIncompatibleModsExtremeWarning;
 
-    @Comment(category = "flashback")
-    @Condition(requiredModId = "flashback")
+    @Comment(category = flashbackCategory)
+    @Condition(requiredModId = flashbackModId)
     public static String flashbackSpacer;
 
-    @Comment(category = "flashback")
-    @Condition(requiredModId = "flashback")
+    @Comment(category = flashbackCategory)
+    @Condition(requiredModId = flashbackModId)
     public static String flashbackReplayForceAllowIncompatibleModsExtremeWarning2;
 
-    @Comment(category = "flashback")
-    @Condition(requiredModId = "flashback")
+    @Comment(category = flashbackCategory)
+    @Condition(requiredModId = flashbackModId)
     public static String flashbackSpacer2;
 
-    @Entry(category = "flashback")
-    @Condition(requiredModId = "flashback")
+    @Entry(category = flashbackCategory)
+    @Condition(requiredModId = flashbackModId)
     public static String flashbackReplayForceAllowIncompatibleMods = "";
 }
