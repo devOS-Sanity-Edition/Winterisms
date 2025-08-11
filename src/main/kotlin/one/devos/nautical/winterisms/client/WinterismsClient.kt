f483fae0f0a996d1c244387bab7cc9449da18176
Naz <bluspringindohq@gmail.com>
2025-08-11 09:14:57 +0800

    Fixed Polymorph compatibility

--- src/main/kotlin/one/devos/nautical/winterisms/client/WinterismsClient.kt
+++ src/main/kotlin/one/devos/nautical/winterisms/client/WinterismsClient.kt
@@ -1,5 +1,7 @@
 package one.devos.nautical.winterisms.client
 
+import com.illusivesoulworks.polymorph.api.client.PolymorphWidgets
+import com.illusivesoulworks.polymorph.api.client.widgets.FurnaceRecipesWidget
 import net.fabricmc.api.ClientModInitializer
 import net.fabricmc.fabric.api.resource.ResourceManagerHelper
 import net.fabricmc.fabric.api.resource.ResourcePackActivationType
@@ -7,6 +9,7 @@ import net.fabricmc.loader.api.FabricLoader
 import net.minecraft.network.chat.Component
 import net.minecraft.resources.ResourceLocation
 import one.devos.nautical.winterisms.Winterisms
+import techreborn.client.gui.GuiElectricFurnace
 
 object WinterismsClient : ClientModInitializer {
     override fun onInitializeClient() {
@@ -16,5 +19,13 @@ object WinterismsClient : ClientModInitializer {
             Component.literal("Alt Raccoon Trinket Texture"),
             ResourcePackActivationType.NORMAL
         )
+
+        PolymorphWidgets.getInstance().registerWidget { screen ->
+            if (screen is GuiElectricFurnace) {
+                return@registerWidget FurnaceRecipesWidget(screen)
+            }
+
+            null
+        }
     }
 }
