90f8b7f4aebb9acdaa335caa4d5edd3cc0437862
Asoji <asoji-git@hiitsdevin.mozmail.com>
2025-08-24 06:58:57 -0700

    improved code for restart cmd [thanks @Deftu]

--- src/main/kotlin/one/devos/nautical/winterisms/commands/RestartCommand.kt
+++ src/main/kotlin/one/devos/nautical/winterisms/commands/RestartCommand.kt
@@ -6,15 +6,23 @@ import de.phyrone.brig.wrapper.literal
 import de.phyrone.brig.wrapper.runs
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents
 import net.minecraft.ChatFormatting
+import net.minecraft.commands.CommandSource
 import net.minecraft.commands.CommandSourceStack
 import net.minecraft.network.chat.Component
 
 val ERROR_FAILED = SimpleCommandExceptionType(Component.translatable("commands.save.failed"))
 
+val REBOOT_TEMPLATE = "Rebooting in %s second(s)"
+
+private fun Int.rebooting(): Component {
+    return Component.literal(REBOOT_TEMPLATE.format(this.toString()))
+}
+
 fun restartCommand(dispatcher: CommandDispatcher<CommandSourceStack>) {
     var ticks = 0
     var startCountdown = false
     var didServerSaveCorrectly = false
+    var lastSource: CommandSourceStack? = null
 
     dispatcher.literal("restart") {
         require { hasPermission(4) }
@@ -25,31 +33,34 @@ fun restartCommand(dispatcher: CommandDispatcher<CommandSourceStack>) {
             if (didServerSaveCorrectly) {
                 it.source.sendSuccess({ Component.literal("Saved server!").withStyle(ChatFormatting.GREEN) }, true)
                 ticks = 15 * 20
-
-                ServerTickEvents.END_SERVER_TICK.register { server ->
-                    ticks--
-
-                    when (ticks) {
-                        295 -> it.source.sendSuccess({ Component.literal("Rebooting in 15 seconds")}, true)
-                        10 * 20 -> it.source.sendSuccess({ Component.literal("Rebooting in 10 seconds")}, true)
-                        5 * 20 -> it.source.sendSuccess({ Component.literal("Rebooting in 5 seconds")}, true)
-                        4 * 20 -> it.source.sendSuccess({ Component.literal("Rebooting in 4 seconds")}, true)
-                        3 * 20 -> it.source.sendSuccess({ Component.literal("Rebooting in 3 seconds")}, true)
-                        2 * 20 -> it.source.sendSuccess({ Component.literal("Rebooting in 2 seconds")}, true)
-                        1 * 20 -> it.source.sendSuccess({ Component.literal("Rebooting in 1 seconds")}, true)
-
-                    }
-                    if (ticks >= 0) {
-                        return@register
-                    }
-
-                    it.source.sendSuccess({ Component.literal("Stopping server...")}, true)
-                    it.source.server.halt(false)
-                }
+                lastSource = it.source
+                it.source.sendSuccess({ 15.rebooting() }, true)
             } else {
                 it.source.sendSuccess({ Component.literal("Server failed to save!").withStyle(ChatFormatting.RED) }, true)
                 throw ERROR_FAILED.create()
             }
         }
+    }
+
+    ServerTickEvents.END_SERVER_TICK.register { server ->
+        ticks--
+
+        val message = when (ticks) {
+            10 * 20 -> 10.rebooting()
+            5 * 20 -> 5.rebooting()
+            4 * 20 -> 4.rebooting()
+            3 * 20 -> 3.rebooting()
+            2 * 20 -> 2.rebooting()
+            1 * 20 -> 1.rebooting()
+            else -> {}
+        }
+
+        lastSource?.sendSuccess({ message }, true)
+        if (ticks >= 0) {
+            return@register
+        }
+
+        lastSource?.sendSuccess({ Component.literal("Stopping server...")}, true)
+        lastSource?.server?.halt(false)
     }
 }
